025870fb106b447add0cccbdfc7e3199ff0c5cf8
albert-zhong <albertzhong@hotmail.com>
2019-09-08 02:33:31 -0700

    Rename personality-usa to seagull, update requirements.txt

---
+++ seagull/codes.py
@@ -0,0 +1,4 @@
+consumer_key = "hDLnGiTM9tP6uTiUnRGPHfBXS"
+consumer_secret = "5dEq74K3nTYZupSjykQK0RKrfZBgFhnZD8ivxBLTQqWnxcnjRW"
+access_token = "1085768253408894977-jBsocMRiVc2rAP7vXlI3zK6PqLDP5l"
+access_secret = "Pkajw56w6czUP13F2QWUegZSNo7jEBe1y392nk6reEjoq"
